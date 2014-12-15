ceb0e9a431e5886cca15f09a6e250e26625c86c9
Costas Voglis <voglinio@gmail.com>
2014-12-15 08:01:33 +0200

    Test commit
    This is a test commit. Just added some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Cource 
+## Prorgamming Assignement 2
+## Costas Voglis
+##
+## In this assignement we are asked to create a wrapper structure
+## around a matrix object that will store its inverse and persist
+## it until we change the matrix. We will follow the implementation
+## of the example code makeVector
 
 ## Write a short comment describing this function
 
